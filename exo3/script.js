0d4d09a15c46bf3f899210d902a14a3e61ee456f
Nzingani <isabelnzingani@outlook.com>
2018-06-20 20:58:51 +0200

    correction

--- exo3/script.js
+++ exo3/script.js
@@ -1,3 +1,3 @@
-
-var Prénom = "Isabel";
-alert(Prénom);
+// Affichage d'une boîte de dialogue permettant de saisir son prénom
+var firstname = prompt= ('Entrez votre prénom : ');
+alert('Bonjour, ' + firstname);
