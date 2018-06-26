2335fa44d0a550858b38962eaab040760b406a4c
Nzingani <isabelnzingani@outlook.com>
2018-06-26 11:46:09 +0200

    first commit

---
+++ JSP1.3/assets/js/script.js
@@ -0,0 +1,9 @@
+function changeColor(){
+document.getElementById('text').style.color = 'green';
+}
+function primaryColor(){
+document.getElementById('text').style.color = 'red';
+}
+function SecondarycColor(){
+document.getElementById('text').style.color = 'blue';
+}
